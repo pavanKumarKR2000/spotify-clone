0c0d8c318d97d4f41acc9e447c3109052ffc951c
Pavankumar K R <pavan123456pop@gmail.com>
2023-09-03 20:56:00 +0530

    added useLoadSongUrl hook

--- components/Player.tsx
+++ components/Player.tsx
@@ -1,8 +1,13 @@
 "use client";
 
+import useGetSongById from "@/hooks/useGetSongById";
+import usePlayer from "@/hooks/usePlayer";
 import React from "react";
 
 const Player = () => {
+  const { activeId } = usePlayer();
+  const { isLoading, song } = useGetSongById(activeId);
+
   return <div>Player</div>;
 };
 
