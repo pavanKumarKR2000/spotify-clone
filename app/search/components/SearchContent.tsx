cf9819ef2f39d575cf396dc007a5eba7f73947cb
Pavankumar K R <pavan123456pop@gmail.com>
2023-09-04 19:07:51 +0530

    added music playing functionality

--- app/search/components/SearchContent.tsx
+++ app/search/components/SearchContent.tsx
@@ -2,6 +2,7 @@
 
 import LikeButton from "@/components/LikeButton";
 import MediaItem from "@/components/MediaItem";
+import useOnPlay from "@/hooks/useOnPlay";
 import { Song } from "@/types";
 import React from "react";
 
@@ -10,6 +11,8 @@ interface SearchContentParams {
 }
 
 const SearchContent: React.FC<SearchContentParams> = ({ songs }) => {
+  const onPlay = useOnPlay(songs);
+
   if (!songs.length) {
     return (
       <div className="flex flex-col gap-y-2 w-full px-6 text-neutral-400">
@@ -23,7 +26,12 @@ const SearchContent: React.FC<SearchContentParams> = ({ songs }) => {
       {songs.map((song) => (
         <div key={song.id} className="flex items-center gap-x-4 w-full">
           <div className="flex-1">
-            <MediaItem data={song} onClick={(id: string) => {}} />
+            <MediaItem
+              data={song}
+              onClick={(id: string) => {
+                onPlay(id);
+              }}
+            />
           </div>
           {/** add like button here */}
           <LikeButton songId={song.id} />
--- components/Library.tsx
+++ components/Library.tsx
@@ -8,6 +8,7 @@ import { useUser } from "@/hooks/useUser";
 import useUploadModal from "@/hooks/useUploadModal";
 import { Song } from "@/types";
 import MediaItem from "./MediaItem";
+import useOnPlay from "@/hooks/useOnPlay";
 
 interface LibraryProps {
   songs: Song[];
@@ -17,6 +18,8 @@ const Library: React.FC<LibraryProps> = ({ songs }) => {
   const { onOpen } = useAuthModal();
   const { user } = useUser();
   const { onOpen: onOpenUpload } = useUploadModal();
+
+  const onPlay = useOnPlay(songs);
 
   const onClickHandler = () => {
     if (!user) {
@@ -41,7 +44,13 @@ const Library: React.FC<LibraryProps> = ({ songs }) => {
       </div>
       <div className="flex flex-col gap-y-2  mt-4 px-3">
         {songs.map((song) => (
-          <MediaItem onClick={() => {}} key={song.id} data={song} />
+          <MediaItem
+            onClick={(id: string) => {
+              onPlay(id);
+            }}
+            key={song.id}
+            data={song}
+          />
         ))}
       </div>
     </div>
